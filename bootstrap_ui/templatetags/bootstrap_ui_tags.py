45af2dec8b0ade8b862a13e4c80cbee21f474ff4
Timo Rieber <dev@timorieber.de>
2015-07-17 21:03:08 +0200

    Prepared for bootstrap and bootswatch themes

--- bootstrap_ui/templatetags/bootstrap_ui_tags.py
+++ bootstrap_ui/templatetags/bootstrap_ui_tags.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.template import Library, TemplateSyntaxError
 from django.utils.safestring import mark_safe
 from dominate import tags
@@ -6,6 +7,23 @@ from tag_parser.basetags import BaseNode
 
 register = Library()
 
+
+@register.assignment_tag(takes_context=True)
+def get_value_from_session_or_cookie(context, key):
+    request = context['request']
+
+    try:
+        # First check for key in session
+        # If no session is available this throws an AttributeError, if the key is not available in an existing session
+        # this throws a KeyError
+        return request.session[key]
+    except (AttributeError, KeyError):
+        try:
+            # Second check for key in cookies
+            return request.COOKIES[key]
+        except KeyError:
+            # Third fallback to settings or return default value
+            return getattr(settings, key, '')
 
 @template_tag(register, 'htmltag')
 class HtmlTagNode(BaseNode):
